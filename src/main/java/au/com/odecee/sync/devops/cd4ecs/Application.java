d3a50f5d7e9bc4b481a07c617d401d3759b3e400
Oliver Lade <olade@odecee.com.au>
2015-12-15 10:07:33 +1100

    Refactored REST API to controller.

--- src/main/java/au/com/odecee/sync/devops/cd4ecs/Application.java
+++ src/main/java/au/com/odecee/sync/devops/cd4ecs/Application.java
@@ -2,8 +2,6 @@ package au.com.odecee.sync.devops.cd4ecs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Application {
@@ -12,8 +10,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @RequestMapping("/hello/{name}")
-    public String hello(@PathVariable String name) {
-        return String.format("Hello, %s!", name == null ? name : "there");
-    }
 }
