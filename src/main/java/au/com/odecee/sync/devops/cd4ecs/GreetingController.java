14a634ab2de2e25bbeb4b7f91afce31b8140a51e
Oliver Lade <olade@odecee.com.au>
2015-12-15 10:37:44 +1100

    Moved app version to app properties, included in API.

--- src/main/java/au/com/odecee/sync/devops/cd4ecs/GreetingController.java
+++ src/main/java/au/com/odecee/sync/devops/cd4ecs/GreetingController.java
@@ -1,5 +1,6 @@
 package au.com.odecee.sync.devops.cd4ecs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,14 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello, %s! (version %s)";
     private final AtomicLong counter = new AtomicLong();
+
+    @Value("${version}")
+    private String version;
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(template, name, version));
     }
 }
